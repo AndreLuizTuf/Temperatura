9378c5c2b333f88b60d3049abbb438aa3a5bddfb
André Luiz <andre.luiz.a.f@hotmail.com>
2023-02-04 11:19:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var temperatura = prompt("Olá! Qual a temperatura atual?");
+
+var corpo = document.getElementById("corpo");
+var h1 = document.getElementById("texto");
+
+if (temperatura >= 26) {
+  h1.innerText = "Está calor!";
+  corpo.style.color = "yellow";
+  corpo.style.backgroundColor = "red";
+} else {
+  h1.innerText = "Está frio!";
+  corpo.style.color = "white";
+  corpo.style.backgroundColor = "blue";
+}
